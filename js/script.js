787fc199018c4890f5d825f4fd6c9d554cc47f06
boyuan5022 <boyuan5022@gmail.com>
2016-08-18 15:19:41 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
-
 $(document).ready(function() {
 	$('#maintitle').scroll(function() {
 		var scroll = $(this).scrollTop();
 		var ht = $('#titleimage').outerHeight();
 		if (scroll >ht){
-			$('#navtitle').css({background: "white",
-							    "border-color": "#CCCCCC"});
-			$('#navtitle a').css({color: "blue"});}
+			$('#navtitle').css({background: 'white',
+							    'border-color': '#CCCCCC'});
+			$('#navtitle a').css({color: 'blue'});}
 		else{
-			$('#navtitle').css({background: "transparent",
-							    "border-color": "transparent"});
-			$('#navtitle a').css({color: "white"});
-		}
+			$('#navtitle').css({background: 'transparent',
+							    'border-color': 'transparent'});
+			$('#navtitle a').css({color: 'white'});}
 		});
 });
