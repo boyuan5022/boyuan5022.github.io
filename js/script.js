2b9290c977f51d0cc92f1e89bea2a13631b4341a
boyuan5022 <boyuan5022@gmail.com>
2016-08-19 11:14:39 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,10 +5,12 @@ $(document).ready(function() {
 		if (scroll >ht){
 			$('#navtitle').css({background: 'white',
 							    'border-color': '#CCCCCC'});
-			$('#navtitle a').css({color: 'blue'});}
+			$('#navtitle a').css({color: 'blue',
+								'border-color': 'blue'});}
 		else{
 			$('#navtitle').css({background: 'transparent',
 							    'border-color': 'transparent'});
-			$('#navtitle a').css({color: 'white'});}
+			$('#navtitle a').css({color: 'white',
+								'border-color': 'white'});}
 		});
 });
