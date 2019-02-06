ca62389142f1c6c8e5a4ee6333f36b7f1e9e3aa6
boyuan5022 <boyuan5022@gmail.com>
2019-02-06 18:52:16 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,26 +1,23 @@
 $(document).ready(function() {
 	$(document).scroll(function() {
 		var scroll = $(this).scrollTop();
-		if (scroll >10 && scroll<$("#imagetitle").outerHeight()*0.8-$('#articletitle').outerHeight()-50){
+		if (scroll >10){
 			$('#navtitle').css({background: 'white',
 							    'border-color': '#CCCCCC'});
-			$('#navtitle a').css({color: 'blue',
-								'border-color': 'blue'});
-			$('#articletitle').css({'position': 'fixed',
-								'top': '20%'});}
-		else if (scroll >$("#imagetitle").outerHeight()*0.8-$('#articletitle').outerHeight()-50){
-			$('#navtitle').css({background: 'white',
-							    'border-color': '#CCCCCC'});
-			$('#navtitle a').css({color: 'blue',
-								'border-color': 'blue'});
-			$('#articletitle').css({'position': 'absolute',
-									'top': $("#imagetitle").outerHeight()-$('#articletitle').outerHeight()-50});}
+			$('#navtitle a').css({color: 'black',
+								'border-color': 'black'});}
 		else{
 			$('#navtitle').css({background: 'transparent',
 								'border-color': 'transparent'});
 			$('#navtitle a').css({color: 'white',
-								'border-color': 'white'});
-			$('#articletitle').css({'position': 'fixed',
-									'top': '20%'});}
+								'border-color': 'white'});}
 		});
+});
+
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    $(".fade").each(function() {
+	  $(this).css("opacity", ($(this).outerHeight()-$( window ).scrollTop( ))/$(this).outerHeight())
+    });
+  }).scroll(); //invoke scroll-handler on page-load
 });
